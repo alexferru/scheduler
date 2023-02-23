747930c141cdea9b81b6dd689dd0045caad5857f
JimHwkins <howlarrior@gmail.com>
2023-02-23 00:13:54 -0300

    Build Appointment components Show, Confirm, Status and Error

--- stories/index.js
+++ stories/index.js
@@ -1,25 +1,20 @@
 import React from "react";
-
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-
 import "index.scss";
 
 import Button from "components/Button";
-
 import DayListItem from "components/DayListItem";
-
 import DayList from "components/DayList";
-
 import InterviewerListItem from "components/InterviewerListItem";
-
 import InterviewerList from "components/InterviewerList";
-
 import Appointment from "components/Appointment/index.js";
-
 import Header from "components/Appointment/Header";
-
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 // Button
 
@@ -158,3 +153,49 @@ storiesOf("Appointment", module)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
   .add("Empty", () => <Empty onAdd={action("onAdd")} />);
+
+// Show
+
+storiesOf("Show", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }],
+  })
+  .add("Show", () => (
+    <Show
+      student="Lydia Miller-Jones"
+      interviewer={interviewer}
+      onEdit={action("onEdit")}
+      onDelete={action("onDelete")}
+    />
+  ));
+
+// Confirm
+
+storiesOf("Confirm", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }],
+  })
+  .add("Confirm", () => (
+    <Confirm
+      message="Delete the appointment?"
+      onConfirm={action("onConfirm")}
+      onCancel={action("onCancel")}
+    />
+  ));
+
+// Status
+
+storiesOf("Status", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }],
+  })
+  .add("Status", () => <Status message="Deleting..." />);
+
+// Error
+
+storiesOf("Error", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }],
+  })
+  .add("Error", () => <Error message="Could not delete appointment." />)
+  .add("Error", () => <Error onClose={action("onClose")} />);
