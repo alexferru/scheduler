30c925ab5015bfa2bab3473a34093e1ae0670bd9
JimHwkins <howlarrior@gmail.com>
2023-02-23 19:47:37 -0300

    Add render for mock appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,8 @@
 import "components/Application.scss";
+import React, { useState } from "react";
 
 import DayList from "./DayList";
-
-import React, { useState } from "react";
+import Appointment from "./Appointment";
 
 const days = [
   {
@@ -21,6 +21,45 @@ const days = [
     spots: 0,
   },
 ];
+
+const appointments = {
+  1: {
+    id: 1,
+    time: "12pm",
+  },
+  2: {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      },
+    },
+  },
+  3: {
+    id: 3,
+    time: "2pm",
+  },
+  4: {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      },
+    },
+  },
+  5: {
+    id: 5,
+    time: "4pm",
+  },
+};
 
 export default function Application(props) {
   const [value, onChange] = useState("Monday");
@@ -43,7 +82,10 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {Object.values(appointments).map((appointment) => (
+          <Appointment key={appointment.id} {...appointment} />
+        ))}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
