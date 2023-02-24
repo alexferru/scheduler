a8c251fb43a68cb7386e7692f5897bd6facbc4fc
JimHwkins <howlarrior@gmail.com>
2023-02-24 01:31:31 -0300

    Implement request days from api using axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,9 @@
 import "components/Application.scss";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = {
   1: {
@@ -62,7 +45,15 @@ const appointments = {
 };
 
 export default function Application(props) {
+  const [days, setDays] = useState([]);
   const [value, onChange] = useState("Monday");
+
+  useEffect(() => {
+    axios.get("/api/days").then((response) => {
+      setDays(response.data);
+    });
+  }, []);
+
   return (
     <main className="layout">
       <section className="sidebar">
