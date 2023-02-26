91d0f3c22ec524c5827d9add5217e0bd99126687
JimHwkins <howlarrior@gmail.com>
2023-02-25 23:54:57 -0300

    Implement useVisualMode custom hook for internal navigation of the Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,14 +3,29 @@ import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import useVisualMode from "hooks/useVisualMode";
+import Form from "./Form";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
   const { time, interview } = props;
+  const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
+
+  const onAdd = () => {
+    transition(CREATE);
+  };
 
   return (
     <article className="appointment">
       <Header time={time} />
-      {interview ? <Show {...interview} /> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={onAdd} />}
+      {mode === SHOW && interview && <Show {...interview} />}
+      {mode === CREATE && (
+        <Form interviewers={[]} onCancel={(back) => transition(EMPTY)} />
+      )}
     </article>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import ReactDOM from "react-dom";
 import "index.scss";
 
 import Application from "components/Application";
+import useVisualMode from "hooks/useVisualMode";
+
 
 ReactDOM.render(<Application />, document.getElementById("root"));
